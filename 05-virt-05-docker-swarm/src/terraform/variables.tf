dc2ba4431792d6dd27f33bb5a36d81e38fe60c74
Denis Borchev <93082167+dborchev@users.noreply.github.com>
2022-05-22 15:08:28 +0000

    prepare terraform 05-virt-05-docker-swarm variables

--- 05-virt-05-docker-swarm/src/terraform/variables.tf
+++ 05-virt-05-docker-swarm/src/terraform/variables.tf
@@ -1,17 +1,17 @@
 # Заменить на ID своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_cloud_id" {
-  default = "b1gu1gt5nqi6lqgu3t7s"
+  default = "b1gsui2eim1uv6p3172b"
 }
 
 # Заменить на Folder своего облака
 # https://console.cloud.yandex.ru/cloud?section=overview
 variable "yandex_folder_id" {
-  default = "b1gaec42k169jqpo02f7"
+  default = "b1gru686jbb7pugd8ed2"
 }
 
 # Заменить на ID своего образа
 # ID можно узнать с помощью команды yc compute image list
 variable "centos-7-base" {
-  default = "fd884f65mo7hpqd0suh4"
+  default = "fd8innv6bfm1jc0u6dvc"
 }
