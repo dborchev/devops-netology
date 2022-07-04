3cdd299b92dc866dda1b137a9dc147b20a6fa254
Denis Borchev <askbow@users.noreply.github.com>
2022-07-04 09:57:37 +0200

    configure ex2 instance

--- teraform/main.tf
+++ teraform/main.tf
@@ -12,3 +12,20 @@ data "aws_ami" "ubuntu" {
         values = ["hvm"]
   }
 }
+
+resource "aws_instance" "netology" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t2.micro"
+  associate_public_ip_address = False
+  availability_zone = "eu-west-1a"
+  ipv6_address_count = "4"
+  source_dest_check = False
+  monitoring = False
+
+  user_data = "apt-get -y update && apt-get -y install nginx"
+
+  tags = {
+    Name = "07-terraform-02-syntax"
+    project = "netology"
+  }
+}
