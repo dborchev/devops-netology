c6af57ec4d28e53a76046f64ad3d673f124d9e7a
Denis Borchev <askbow@users.noreply.github.com>
2022-07-04 09:42:13 +0200

    automatically select ubuntu ami

--- teraform/main.tf
+++ teraform/main.tf
@@ -2,3 +2,13 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+  name_regex       = "^ubuntu/images/hvm-ssd/ubuntu-jammy-.+"
+  owners = ["099720109477"] # Canonical https://ubuntu.com/server/docs/cloud-images/amazon-ec2
+
+  filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+  }
+}
