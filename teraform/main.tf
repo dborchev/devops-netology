1d560f7722227fd53b058d57e51ed5beafc3e409
Denis Borchev <askbow@users.noreply.github.com>
2022-07-04 10:11:49 +0200

    add outputs

--- teraform/main.tf
+++ teraform/main.tf
@@ -2,6 +2,9 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
 data "aws_ami" "ubuntu" {
   most_recent      = true
   name_regex       = "^ubuntu/images/hvm-ssd/ubuntu-jammy-.+"
@@ -13,16 +16,53 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+resource "aws_vpc" "netology_vpc" {
+  cidr_block = "172.16.0.0/16"
+
+  tags = {
+    Name = "tf-example"
+    project = "netology"
+  }
+}
+
+resource "aws_subnet" "netology_subnet" {
+  vpc_id            = aws_vpc.netology_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "tf-example"
+    project = "netology"
+  }
+}
+
+resource "aws_network_interface" "netology_interface" {
+  subnet_id   = aws_subnet.netology_subnet.id
+  private_ips = ["172.16.10.100"]
+
+  tags = {
+    Name = "primary_network_interface"
+  }
+}
+
+
+
 resource "aws_instance" "netology" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
-  associate_public_ip_address = False
   availability_zone = "eu-west-1a"
-  ipv6_address_count = "4"
-  source_dest_check = False
   monitoring = False
 
   user_data = "apt-get -y update && apt-get -y install nginx"
+
+  network_interface = {
+    network_interface_id = aws_network_interface.netology_interface.id
+    device_index = 0
+  }
+
+  credit_specification = {
+    cpu_credits = "unlimited"
+  }
 
   tags = {
     Name = "07-terraform-02-syntax"
