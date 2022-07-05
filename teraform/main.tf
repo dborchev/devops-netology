fd6dfe9650a788054d5f795d561272ba04c604c4
Denis Borchev <askbow@users.noreply.github.com>
2022-07-05 08:45:07 +0200

    extend config to use workspaces

--- teraform/main.tf
+++ teraform/main.tf
@@ -38,7 +38,7 @@ resource "aws_subnet" "netology_subnet" {
 
 resource "aws_network_interface" "netology_interface" {
   subnet_id   = aws_subnet.netology_subnet.id
-  private_ips = ["172.16.10.100"]
+  count = terraform.workspace == "prod" ? 2 : 1
 
   tags = {
     Name = "primary_network_interface"
@@ -49,14 +49,15 @@ resource "aws_network_interface" "netology_interface" {
 
 resource "aws_instance" "netology" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  instance_type = terraform.workspace == "prod" ? "t2.micro" : "t1.micro"
   availability_zone = "eu-west-1a"
   monitoring = "false"
-
+  count = terraform.workspace == "prod" ? 2 : 1
+  create_before_destroy = true
   user_data = "apt-get -y update && apt-get -y install nginx"
 
   network_interface {
-    network_interface_id = aws_network_interface.netology_interface.id
+    network_interface_id = aws_network_interface.netology_interface[count.index].id
     device_index = 0
   }
 
@@ -65,7 +66,33 @@ resource "aws_instance" "netology" {
   }
 
   tags = {
-    Name = "07-terraform-02-syntax"
+    Name = "07-terraform-${terraform.workspace}-${count.index}"
+    project = "netology"
+  }
+}
+
+
+locals {
+  prod_vm = {
+    "vm1" = {size = "t2.micro", zone="eu-west-1a"},
+    "vm1" = {size = "t2.micro", zone="eu-west-1b"}
+  }
+  stage_vm = {
+    "vm1" = {size = "t1.micro", zone="eu-west-1c"},
+    }
+}
+
+resource "aws_instance" "netology_for_each" {
+  for_each = terraform.workspace == "prod" ? local.prod_vm : local.stage_vm
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = each.value.size
+  availability_zone = each.value.zone
+  monitoring = "false"
+
+  user_data = "apt-get -y update && apt-get -y install nginx"
+
+  tags = {
+    Name = "07-terraform-${terraform.workspace}-${each.key}"
     project = "netology"
   }
 }
